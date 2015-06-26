6b3cf03dd50a4482b7a447904930bb8d787187a0
zzqiltw <zzqiltw@gmail.com>
2015-06-26 12:53:30 +0800

    ocr测试，但是百度识别的效果并不好

--- MT/ZQAppDelegate.m
+++ MT/ZQAppDelegate.m
@@ -18,6 +18,11 @@
     return YES;
 }
 
+- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application
+{
+    NSLog(@"内存警告！！");
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- MT/ZQViewController.m
+++ MT/ZQViewController.m
@@ -10,11 +10,12 @@
 #import "ZQTranslateViewController.h"
 #import "ZQNavigationButton.h"
 
-@interface ZQViewController ()
+@interface ZQViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 - (IBAction)cn2En:(ZQNavigationButton *)sender;
 
 - (IBAction)en2Cn:(ZQNavigationButton *)sender;
 
+- (IBAction)ocrTest:(id)sender;
 
 
 @end
@@ -39,6 +40,64 @@
 - (IBAction)en2Cn:(ZQNavigationButton *)sender {
     [self pushToTranslateVC:TranslateTypeEn2Cn];
 }
+
+- (IBAction)ocrTest:(id)sender {
+//    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeCamera;
+//    //判断是否有摄像头
+//    if(![UIImagePickerController isSourceTypeAvailable:sourceType])
+//    {
+//        sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+//    }
+
+    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+    UIImagePickerController *imagePickerController = [[UIImagePickerController alloc] init];
+    imagePickerController.delegate = self;   // 设置委托
+    imagePickerController.sourceType = sourceType;
+    imagePickerController.allowsEditing = YES;
+    [self presentViewController:imagePickerController animated:YES completion:nil];  //需要以模态的形式展示
+}
+
+
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    if (image == nil) {
+        image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    }
+    NSData *imgData = UIImageJPEGRepresentation(image, 0.0001);
+    NSString* encodeResult = [imgData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+//    encodeResult = [encodeResult stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *httpUrl = @"http://apis.baidu.com/apistore/idlocr/ocr";
+    NSString *httpArg = [NSString stringWithFormat:@"fromdevice=iPhone&clientip=10.10.10.0&detecttype=LocateRecognize&languagetype=CHN_ENG&imagetype=1&image=%@", encodeResult];
+    [self request: httpUrl withHttpArg: httpArg];
+    
+    [picker dismissViewControllerAnimated:YES completion:^{}];
+}
+
+-(void)request: (NSString*)httpUrl withHttpArg: (NSString*)HttpArg  {
+    NSURL *url = [NSURL URLWithString: httpUrl];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]initWithURL: url cachePolicy: NSURLRequestUseProtocolCachePolicy timeoutInterval: 10];
+    [request setHTTPMethod: @"POST"];
+    [request addValue: @"dfd35e6ce265322a3129da6005dc1a8c" forHTTPHeaderField: @"apikey"];
+    [request addValue: @"application/x-www-form-urlencoded" forHTTPHeaderField: @"Content-Type"];
+    NSData *data = [HttpArg dataUsingEncoding: NSUTF8StringEncoding];
+    [request setHTTPBody: data];
+    [NSURLConnection sendAsynchronousRequest: request
+                                       queue: [NSOperationQueue mainQueue]
+                           completionHandler: ^(NSURLResponse *response, NSData *data, NSError *error){
+                               if (error) {
+                                   NSLog(@"Httperror: %@%ld", error.localizedDescription, (long)error.code);
+                               } else {
+                                   NSInteger responseCode = [(NSHTTPURLResponse *)response statusCode];
+                                   NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                                   NSLog(@"HttpResponseCode:%ld", (long)responseCode);
+                                   NSLog(@"HttpResponseBody %@",responseString);
+                               }
+                           }];
+}
+
 
 - (void)pushToTranslateVC:(TranslateType)type
 {
